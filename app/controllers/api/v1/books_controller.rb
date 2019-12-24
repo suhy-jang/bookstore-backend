1ee45b7bc12157c60f9e0fe31af0d15987bb2f3d
shjang7 <suhy.jang.shj@gmail.com>
2019-12-24 15:48:38 +0900

    Add book api for index, create, delete

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::BooksController < ApiController
+  before_action :set_book, only: [:delete]
+
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book
+    end
+  end
+
+  def delete
+    @book.destroy
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :description)
+  end
+end
