03779b8325cb68914b3ba464e47c6e8cba846da4
shjang7 <suhy.jang.shj@gmail.com>
2020-02-03 20:34:23 +0900

    Add error handling

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,16 +3,35 @@ class Api::V1::BooksController < ApiController
 
   def index
     @books = Book.all
-    render json: @books
+    render json: { books: @books }
   end
 
   def create
-    @book = Book.create(book_params)
-    render json: @book
+    @book = Book.new(book_params)
+    if @book.save
+      render json: {
+        status: :created,
+        location: @book
+      }
+    else
+      render json: {
+        status: :fail,
+        errors: @book.errors.full_messages
+      }
+    end
   end
 
   def destroy
-    @book.destroy
+    if @book.destroy
+      render json: {
+        status: :destroyed
+      }
+    else
+      render json: {
+        status: :fail,
+        errors: ['failed delete']
+      }
+    end
   end
 
   private
